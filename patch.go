24a0d8f1af0c3990fc404f074f2cfc3ac4e76d69
Alexander Lokhman <alex.lokhman@gmail.com>
2018-03-23 10:38:06 +0000

    Remove commented code

--- patch.go
+++ patch.go
@@ -13,14 +13,6 @@ func direct(p uintptr, length int) []byte {
 	}))
 }
 
-/* func patch(src, dest uintptr) ([]byte, error) {
-	jb := jmp(dest)
-	raw := direct(src, len(jb))
-	bkp := make([]byte, len(raw))
-	copy(bkp, raw)
-	return bkp, write(src, jb)
-} */
-
 func patch(p, p0 uintptr) error {
 	return write(p, jmp(p0))
 }
